ab228c12b11e37b041803ee1b0c31fe06eb56fd2
Andrew <andrewriznykua93@gmail.com>
2019-08-28 00:14:27 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+class Song
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_count = {}
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@artists.push(artist)
+    @@genres.push(genre)
+    @@genre_count[genre] ? @@genre_count[genre] += 1 : @@genre_count[genre] = 1
+    @@artist_count[artist] ? @@artist_count[artist] += 1 : @@artist_count[artist] = 1
+    @name = name
+    @artist = artist
+    @genre = genre
+  end  
+
+  def self.count
+    @@count
+  end  
+ 
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    @@genre_count
+  end
+  
+  def self.artist_count
+    @@artist_count
+  end
+
+end
